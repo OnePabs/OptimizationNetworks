54c4956e8922cb076839f0ff0d835c77a875414d
Juan Contreras <juancontreras@localhost.localdomain>
2021-04-23 14:15:49 -0400

    added shortest path solver and put files in parent folder

---
+++ shortest-path-solver.py
@@ -0,0 +1,10 @@
+# Solve a problem of Shortest path 
+# given an undirected graph
+
+from UndirectedGraph import UndirectedGraph
+
+#create graph
+g1 = UndirectedGraph('g1')
+g1.createGraph
+g1.printGraph
+
---
+++ shortest-path/shortest-path-solver.py
@@ -0,0 +1,9 @@
+# Solve a problem of Shortest path 
+# given an undirected graph
+
+from 
+#create graph
+g1 = UndirectedGraph('g1')
+g1.createGraph
+g1.printGraph
+
